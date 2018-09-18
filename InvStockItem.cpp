f51e9485e513083d276c3f3c20deb87fa1db5f09
Chris Hanagan <chris.hanagan@gmail.com>
2018-09-18 05:51:08 -0400

    split out the class fields

--- InvStockItem.cpp
+++ InvStockItem.cpp
@@ -17,6 +17,7 @@ size_t InvStockItem::SetFirst(size_t line_idx) {
     size_t wrk_idx = line_idx;
     size_t strlen, lines_used;
     int loop_idx = 0;
+    lines_used = 0;
 
     line_in = inventory->inv_line(wrk_idx);
 
@@ -25,17 +26,32 @@ size_t InvStockItem::SetFirst(size_t line_idx) {
 
     if (line_parts[0] == "(LIN)") {
         temp_string = line_parts[1];
-        lines_used += 1;
+        lines_used ++;
     }
 
     string_idx = "none";
     do {
-        line_parts = wxSplit(inventory->inv_line(wrk_idx), ' ');
+        line_parts = wxSplit(inventory->inv_line(wrk_idx++), ' ');
         string_idx = line_parts[0];
-        lines_used += 1;
+        if (string_idx == "(LIN)") {
+            lin = string_idx;
+        }
+        if (string_idx == "(MPO") {
+            mpo = string_idx;
+        }
+        if (string_idx == "(NOM)") {
+            nomen = string_idx;
+        }
+        if (string_idx == "(MATNR)") {
+            niin = string_idx;
+        }
+        if (string_idx == "(QTY)") {
+            quantity = string_idx;
+        }
+        lines_used ++;
     } while (string_idx != "(LIN)");
 
-    wrk_idx += lines_used;
+//    wrk_idx += lines_used;
 //
 //            if (line_parts.Item(0).Cmp("FE:")) {
 //                continue;
