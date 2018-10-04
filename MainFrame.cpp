46cf709fb1b80db643fe97f451c080b7cffb3747
Chris Hanagan <chris.hanagan@gmail.com>
2018-10-04 06:18:15 -0400

    Clean up menu
    Issue with menu quit and ID_Exit

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -7,124 +7,127 @@
 #include "InventoryDocument.h"
 //#include "InvStockItem.h"
 
-MainFrame::MainFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
-        :wxFrame((wxFrame*) NULL, -1, title, pos, size)
-{
-    CreateStatusBar(2);
-    MainMenu = new wxMenuBar();
-    wxMenu* MenuFile = new wxMenu();
+MainFrame::MainFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
+    : wxFrame((wxFrame *) NULL, -1, title, pos, size) {
+  MainMenu = new wxMenuBar();
+  wxMenu *MenuFile = new wxMenu();
 
-    MenuFile->Append(MENU_New, wxT("&New"),
-            wxT("Create a new file"));
-    MenuFile->Append(MENU_Open, wxT("&Open"),
-            wxT("Open an existing File"));
-    MenuFile->Append(MENU_Quit, wxT("&Close"),
-            wxT("Close the current document"));
-    MenuFile->Append(MENU_Save, wxT("&Save"),
-            wxT("Save the current document"));
-    MenuFile->Append(MENU_SaveAs, wxT("Save &As"),
-            wxT("Save the current document under a new file name"));
-    MenuFile->Append(MENU_Quit, wxT("&Quit"),
-            wxT("Quit the editor"));
+  MenuFile->Append(MENU_New, wxT("&New"),
+                   wxT("Create a new file"));
+  MenuFile->Append(MENU_Open, wxT("&Open"),
+                   wxT("Open an existing File"));
+  MenuFile->Append(MENU_Quit, wxT("&Close"),
+                   wxT("Close the current document"));
+  MenuFile->Append(MENU_Save, wxT("&Save"),
+                   wxT("Save the current document"));
+  MenuFile->Append(MENU_SaveAs, wxT("Save &As"),
+                   wxT("Save the current document under a new file name"));
 
-    MainMenu->Append(MenuFile, wxT("File"));
-    SetMenuBar(MainMenu);
+    MenuFile->AppendSeparator();
+  MenuFile->Append(wxID_EXIT, wxT("&Quit"),
+//  MenuFile->Append(MENU_Quit, wxT("&Quit"),
+                   wxT("Quit the editor"));
 
-    MainEditBox = new wxTextCtrl(this, TEXT_Main, "",
-            wxDefaultPosition, wxDefaultSize,
-            wxTE_MULTILINE | wxTE_RICH,
-            wxDefaultValidator, wxTextCtrlNameStr);
+  MainMenu->Append(MenuFile, wxT("File"));
+  SetMenuBar(MainMenu);
+
+  MainEditBox = new wxTextCtrl(this, TEXT_Main, "",
+                               wxDefaultPosition, wxDefaultSize,
+                               wxTE_MULTILINE | wxTE_RICH,
+                               wxDefaultValidator, wxTextCtrlNameStr);
+  CreateStatusBar(2);
+  SetStatusText(_("Here we are"), 1);
+
+  Bind(wxEVT_MENU, &MainFrame::Quit, this, wxID_EXIT);
 //    Maximize();
 }
 
-void MainFrame::NewFile(wxCommandEvent& WXUNUSED(event))
-{
+void MainFrame::NewFile(wxCommandEvent & WXUNUSED(event)) {
 
 }
 
-void MainFrame::OpenFile(wxCommandEvent& event)
-{
-    InventoryDocument* inventory = new InventoryDocument();
-    InvStockItem* tmp_nsn_item;
-    wxString lStr;
-    int number_lines;
-    bool got_file = false;
+void MainFrame::OpenFile(wxCommandEvent &event) {
+  InventoryDocument *inventory = new InventoryDocument();
+  InvStockItem *tmp_nsn_item;
+  wxString lStr;
+  int number_lines;
+  bool got_file = false;
 //    InvStockItem * stockItem;
-    wxArrayString lstr_splt;
-    wxString wrkstr;
-    size_t strlen;
-    size_t line_idx;
-    int num_lines;
-    int num_nsn;
+  wxArrayString lstr_splt;
+  wxString wrkstr;
+  size_t strlen;
+  size_t line_idx;
+  int num_lines;
+  int num_nsn;
 
-    wxFileDialog* OpenDialog = new wxFileDialog(
-            this, _("Choose a file to open"), wxEmptyString, wxEmptyString,
-            _("Text files (*.txt)|*.txt|"
-              "C++ Source Files (*.cpp, *.cxx)|*.cpp;*.cxx|"
-              "C Source files (*.c)|*.c|"
-              "C header files (*.h)|*.h"),
-            wxFD_OPEN, wxDefaultPosition);
+  wxFileDialog *OpenDialog = new wxFileDialog(
+      this, _("Choose a file to open"), wxEmptyString, wxEmptyString,
+      _("Text files (*.txt)|*.txt|"
+        "C++ Source Files (*.cpp, *.cxx)|*.cpp;*.cxx|"
+        "C Source files (*.c)|*.c|"
+        "C header files (*.h)|*.h"),
+      wxFD_OPEN, wxDefaultPosition);
 
-    if (OpenDialog->ShowModal()==wxID_OK) {
+  if (OpenDialog->ShowModal()==wxID_OK) {
 
-        CurrentDocPath = OpenDialog->GetPath();
-        SetTitle(wxString("Edit - ") << OpenDialog->GetFilename());
-        got_file = true;
+    CurrentDocPath = OpenDialog->GetPath();
+    SetTitle(wxString("Edit - ") << OpenDialog->GetFilename());
+    got_file = true;
+  }
+  if (got_file) {
+
+    // load the inventory document
+    inventory->LoadInventory(CurrentDocPath);
+
+    num_lines = (int) inventory->line_count();
+    num_nsn = (int) inventory->nsn_count();
+
+    lStr.Printf(wxT("Document Number: %s\n"), inventory->inv_doc_num());
+    SetStatusText(lStr, 0);
+    (*MainEditBox) << lStr;
+
+    lStr.Printf(wxT("Document Date: %s\n"), inventory->inv_doc_date());
+    SetStatusText(lStr, 1);
+    (*MainEditBox) << lStr;
+
+    lStr.Printf(wxT("Number of lines: %d\n\n"), num_lines);
+    (*MainEditBox) << lStr;
+    lStr.Printf(wxT("Number of NSN Items: %d\n\n"), num_nsn);
+    (*MainEditBox) << lStr;
+
+
+    // display each line
+    if (false) {
+      for (int i = 0; i < num_lines; i++) {
+        lStr.Printf("%s\n", inventory->inv_line((size_t) i));
+        (*MainEditBox) << lStr;
+      }
+
     }
-    if (got_file) {
 
-        // load the inventory document
-        inventory->LoadInventory(CurrentDocPath);
-
-        num_lines = (int) inventory->line_count();
-        num_nsn = (int) inventory->nsn_count();
-
-        lStr.Printf(wxT("Document Number: %s\n"), inventory->inv_doc_num());
-        (*MainEditBox) << lStr;
-        lStr.Printf(wxT("Document Date: %s\n"), inventory->inv_doc_date());
-        (*MainEditBox) << lStr;
-        lStr.Printf(wxT("Number of lines: %d\n\n"), num_lines);
-        (*MainEditBox) << lStr;
-        lStr.Printf(wxT("Number of NSN Items: %d\n\n"), num_nsn);
-        (*MainEditBox) << lStr;
-
-
-        // display each line
-        if (false) {
-            for (int i = 0; i<num_lines; i++) {
-                lStr.Printf("%s\n", inventory->inv_line((size_t) i));
-                (*MainEditBox) << lStr;
-            }
-
-        }
-
-        // display each nsn item
-        for (int i = 0; i<num_nsn; i++) {
-            tmp_nsn_item = inventory->nsn_item((size_t) i);
-            lStr.Printf(wxT("%d: %s\n"), i, tmp_nsn_item->getString());
+    // display each nsn item
+    for (int i = 0; i < num_nsn; i++) {
+      tmp_nsn_item = inventory->nsn_item((size_t) i);
+      lStr.Printf(wxT("%d: %s\n"), i, tmp_nsn_item->getString());
 //            lStr.format("%d: %s\n", i, tmp_nsn_item->getString());
-            (*MainEditBox) << lStr;
-        }
-// TODO invdoc => list of stock_items - not stock_items list of matnr indexes
+      (*MainEditBox) << lStr;
     }
+// TODO invdoc => list of stock_items - not stock_items list of matnr indexes
+  }
 
 }
 
-void MainFrame::CloseFile(wxCommandEvent& event)
-{
-    MainEditBox->Clear();
+void MainFrame::CloseFile(wxCommandEvent &event) {
+  MainEditBox->Clear();
 }
 
-void MainFrame::SaveFile(wxCommandEvent& event)
-{
+void MainFrame::SaveFile(wxCommandEvent &event) {
 }
 
-void MainFrame::SaveFileAs(wxCommandEvent& event)
-{
+void MainFrame::SaveFileAs(wxCommandEvent &event) {
 
 }
 
-void MainFrame::Quit(wxCommandEvent& WXUNUSED(event))
-{
-    Close(TRUE);
+void MainFrame::Quit(wxCommandEvent & WXUNUSED(event)) {
+  Close(TRUE);
 }
